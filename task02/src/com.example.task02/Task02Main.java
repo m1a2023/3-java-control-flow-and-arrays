2be2c2428373a09a5d6c63e6a9c0038a6d57c4a4
m1a2023 <mag2017go@gmail.com>
2024-09-12 17:13:32 +0500

    task02 was done

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -1,6 +1,15 @@
 package com.example.task02;
 
+import java.util.HashMap;
+
 public class Task02Main {
+    enum season {
+        WINTER,
+        SPRING,
+        SUMMER,
+        AUTUMN
+    }
+
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
@@ -10,6 +19,15 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        monthNumber %= 12;
+
+        switch (monthNumber) {
+            case 0: case 1: case 2: return "зима";
+            case 3: case 4: case 5: return "весна";
+            case 6: case 7: case 8: return "лето";
+            case 9: case 10: case 11: return "осень";
+        }
+
+        return "";
     }
 }
