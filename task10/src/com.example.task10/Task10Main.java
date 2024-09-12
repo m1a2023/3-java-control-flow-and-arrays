c2d67239bfbacac86e670f4320e0554e3d214b85
m1a2023 <mag2017go@gmail.com>
2024-09-12 18:55:51 +0500

    task09 was done

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -1,5 +1,7 @@
 package com.example.task10;
 
+import java.util.Arrays;
+
 public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -11,8 +13,15 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int indexMin = 0, minv = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (minv >= arr[i]) {
+                minv = arr[i];
+                indexMin = i;
+            }
+        }
+        return indexMin;
     }
 
 }
